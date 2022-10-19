99249201d8dfd0072297817424160d5fe8b09f6f
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-10-19 20:34:44 -0300

    adhiriendo SEO google

--- pages/Contacto.js
+++ pages/Contacto.js
@@ -8,6 +8,10 @@ const Contacto = () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
+        <meta
+          name="google-site-verification"
+          content="kQCyfV4ZucwsahnLoMCgGrF45OFMD4zj3DksflAVHgo"
+        />
         <title> Contacto | Martin Maffei</title>
         <link rel="shortcut icon" href="/static/medium.ico" />
       </Helmet>
